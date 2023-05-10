6abce33c5e9800932d089c108171ec08be4e037a
Polly <polly19940717@gmail.com>
2023-05-10 16:55:38 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/LinPolly.github.io/'
+    },
     css: [
         'vuetify/lib/styles/main.css',
         '@mdi/font/css/materialdesignicons.min.css'
