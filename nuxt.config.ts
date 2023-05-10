c73e01d8650d6b442c11f424aa4b9391f83d2003
Polly <polly19940717@gmail.com>
2023-05-10 10:21:28 +0800

    u

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    server: {
+        host: '0.0.0.0'
+    },
+    css: [
+        'vuetify/lib/styles/main.css',
+        '@mdi/font/css/materialdesignicons.min.css'
+    ],
+    build: {
+        transpile: ['vuetify'],
+    },
+    modules: [
+    ],
+    components: [
+        {
+            path: '~/components',
+            extensions: ['.vue'],
+        }
+    ],
 })
