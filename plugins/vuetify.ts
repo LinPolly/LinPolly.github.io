f16f83fa4b71519fab9d06b2a28f6df18d546c04
Polly <polly19940717@gmail.com>
2023-05-10 16:26:05 +0800

    Update vuetify.ts

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,12 +4,44 @@ import * as directives from 'vuetify/directives'
 // import { VDataTable, VDataTableServer } from 'vuetify/labs/VDataTable'
 import * as labs from 'vuetify/labs/components'
 
+const light: ThemeDefinition = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        primary: '#6200EE',
+        'primary-darken-1': '#3700B3',
+        secondary: '#03DAC6',
+        'secondary-darken-1': '#018786',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    }
+}
+const dark: ThemeDefinition = {
+    dark: true,
+    colors: {
+        primary: "#52E3C2",
+        "page-header-background": "#282831",
+        "page-background": "#32323E",
+        "table-header": "#2e2e2e",
+        background: "#3F3F4A",
+        "header-background": "#4a4a59",
+        "info-text": "#99999F",
+    }
+}
+
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
         theme: {
-            defaultTheme: 'light'
+            defaultTheme: 'light',
+            themes: {
+                light,
+                dark
+            }
         },
-        ssr: true,
+        // ssr: true,
         components: {
             ...components,
             ...labs,
